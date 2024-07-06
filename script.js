b68b2ea91da3d4f37acb8c98d2d70466ff0ed7b8
M1SHANIA <mihaonipchenko@gmail.com>
2024-07-06 22:24:15 +0200

    finished logic for the spins

--- script.js
+++ script.js
@@ -1,179 +1,135 @@
 const prizes = [
-    {
-      text: "500 ZWM",
-      color: "hsl(199 92% 74%)",
-    },
-    { 
-      text: "Spin Again",
-      color: "hsl(340 82% 76%)",
-    },
-    { 
-      text: "50 ZWM",
-      color: "hsl(199 92% 74%)",
-    },
-    {
-      text: "3000 ZWM",
-      color: "hsl(340 82% 76%)",
-    },
-    {
-      text: "100 ZWM",
-      color: "hsl(199 92% 74%)",
-    },
-    {
-      text: "Spin Again",
-      color: "hsl(340 82% 76%)",
-    },
-    {
-      text: "1000 ZWM",
-      color: "hsl(199 92% 74%)",
-    },
-    {
-      text: "5000 ZWM",
-      color: "hsl(340 82% 76%)",
-    }
-  ];
-  
-  // создаём переменные для быстрого доступа ко всем объектам на странице — блоку в целом, колесу, кнопке и язычку
-  const wheel = document.querySelector(".deal-wheel");
-  const spinner = wheel.querySelector(".spinner");
-  const trigger = wheel.querySelector(".btn-spin");
-  const ticker = wheel.querySelector(".ticker");
-  
-  // на сколько секторов нарезаем круг
-  const prizeSlice = 360 / prizes.length;
-  // на какое расстояние смещаем сектора друг относительно друга
-  const prizeOffset = Math.floor(180 / prizes.length);
-  // прописываем CSS-классы, которые будем добавлять и убирать из стилей
-  const spinClass = "is-spinning";
-  const selectedClass = "selected";
-  // получаем все значения параметров стилей у секторов
-  const spinnerStyles = window.getComputedStyle(spinner);
-  
-  // переменная для анимации
-  let tickerAnim;
-  // угол вращения
-  let rotation = 0;
-  // текущий сектор
-  let currentSlice = 0;
-  // переменная для текстовых подписей
-  let prizeNodes;
-  
-  // расставляем текст по секторам
-  const createPrizeNodes = () => {
-    // обрабатываем каждую подпись
-    prizes.forEach(({ text, color, reaction }, i) => {
-      // каждой из них назначаем свой угол поворота
-      const rotation = ((prizeSlice * i) * -1) - prizeOffset;
-      // добавляем код с размещением текста на страницу в конец блока spinner
-      spinner.insertAdjacentHTML(
-        "beforeend",
-        // текст при этом уже оформлен нужными стилями
-        `<li class="prize" data-reaction=${reaction} style="--rotate: ${rotation}deg">
-          <span class="text">${text}</span>
-        </li>`
-      );
-    });
-  };
-  
-  // рисуем разноцветные секторы
-  const createConicGradient = () => {
-    // устанавливаем нужное значение стиля у элемента spinner
-    spinner.setAttribute(
-      "style",
-      `background: conic-gradient(
-        from -90deg,
-        ${prizes
-          // получаем цвет текущего сектора
-          .map(({ color }, i) => `${color} 0 ${(100 / prizes.length) * (prizes.length - i)}%`)
-          .reverse()
-        }
-      );`
+  {
+    text: "500 ZWM",
+    color: "hsl(200 92% 64%)",
+  },
+  {
+    text: "Spin Again",
+    color: "hsl(317 88% 70%)",
+  },
+  {
+    text: "50 ZWM",
+    color: "hsl(179 75% 50%)",
+  },
+  {
+    text: "3000 ZWM",
+    color: "hsl(294 49% 71%)",
+  },
+  {
+    text: "100 ZWM",
+    color: "hsl(200 92% 64%)",
+  },
+  {
+    text: "Spin Again",
+    color: "hsl(317 88% 70%)",
+  },
+  {
+    text: "1000 ZWM",
+    color: "hsl(179 75% 50%)",
+  },
+  {
+    text: "5000 ZWM",
+    color: "hsl(294 49% 71%)",
+  }
+];
+const wheel = document.querySelector(".deal-wheel");
+const spinner = wheel.querySelector(".spinner");
+const trigger = wheel.querySelector(".btn-spin");
+const ticker = wheel.querySelector(".ticker");
+
+const prizeSlice = 360 / prizes.length;
+const prizeOffset = Math.floor(180 / prizes.length);
+const spinClass = "is-spinning";
+const selectedClass = "selected";
+const spinnerStyles = window.getComputedStyle(spinner);
+
+let tickerAnim;
+let rotation = 0;
+let currentSlice = 0;
+let prizeNodes;
+let spinCount = 0;
+
+const createPrizeNodes = () => {
+  prizes.forEach(({ text, color }, i) => {
+    const rotation = ((prizeSlice * i) * -1) - prizeOffset;
+    spinner.insertAdjacentHTML(
+      "beforeend",
+      `<li class="prize" style="--rotate: ${rotation}deg">
+        <span class="text">${text}</span>
+      </li>`
     );
-  };
-  
-  // создаём функцию, которая нарисует колесо в сборе
-  const setupWheel = () => {
-    // сначала секторы
-    createConicGradient();
-    // потом текст
-    createPrizeNodes();
-    // а потом мы получим список всех призов на странице, чтобы работать с ними как с объектами
-    prizeNodes = wheel.querySelectorAll(".prize");
-  };
-  
-  // определяем количество оборотов, которое сделает наше колесо
-  const spinertia = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-  
-  // функция запуска вращения с плавной остановкой
-  const runTickerAnimation = () => {
-    // взяли код анимации отсюда: https://css-tricks.com/get-value-of-css-rotation-through-javascript/
-    const values = spinnerStyles.transform.split("(")[1].split(")")[0].split(",");
-    const a = values[0];
-    const b = values[1];  
-    let rad = Math.atan2(b, a);
-    
-    if (rad < 0) rad += (2 * Math.PI);
-    
-    const angle = Math.round(rad * (180 / Math.PI));
-    const slice = Math.floor(angle / prizeSlice);
-  
-    // анимация язычка, когда его задевает колесо при вращении
-    // если появился новый сектор
-    if (currentSlice !== slice) {
-      // убираем анимацию язычка
-      ticker.style.animation = "none";
-      // и через 10 миллисекунд отменяем это, чтобы он вернулся в первоначальное положение
-      setTimeout(() => ticker.style.animation = null, 10);
-      // после того, как язычок прошёл сектор - делаем его текущим 
-      currentSlice = slice;
+  });
+};
+
+const createConicGradient = () => {
+  spinner.setAttribute(
+    "style",
+    `background: conic-gradient(
+      from -90deg,
+      ${prizes
+      .map(({ color }, i) => `${color} 0 ${(100 / prizes.length) * (prizes.length - i)}%`)
+      .reverse()
     }
-    // запускаем анимацию
-    tickerAnim = requestAnimationFrame(runTickerAnimation);
-  };
-  
-  // функция выбора призового сектора
-  const selectPrize = () => {
-    const selected = Math.floor(rotation / prizeSlice);
-    prizeNodes[selected].classList.add(selectedClass);
-  };
-  
-  // отслеживаем нажатие на кнопку
-  trigger.addEventListener("click", () => {
-    // делаем её недоступной для нажатия
-    trigger.disabled = true;
-    // задаём начальное вращение колеса
-    rotation = Math.floor(Math.random() * 360 + spinertia(2000, 5000));
-    // убираем прошлый приз
-    prizeNodes.forEach((prize) => prize.classList.remove(selectedClass));
-    // добавляем колесу класс is-spinning, с помощью которого реализуем нужную отрисовку
-    wheel.classList.add(spinClass);
-    // через CSS говорим секторам, как им повернуться
-    spinner.style.setProperty("--rotate", rotation);
-    // возвращаем язычок в горизонтальную позицию
+    );`
+  );
+};
+
+const setupWheel = () => {
+  createConicGradient();
+  createPrizeNodes();
+  prizeNodes = wheel.querySelectorAll(".prize");
+};
+
+const runTickerAnimation = () => {
+  const values = spinnerStyles.transform.split("(")[1].split(")")[0].split(",");
+  const a = values[0];
+  const b = values[1];
+  let rad = Math.atan2(b, a);
+
+  if (rad < 0) rad += (2 * Math.PI);
+
+  const angle = Math.round(rad * (180 / Math.PI));
+  const slice = Math.floor(angle / prizeSlice);
+
+  if (currentSlice !== slice) {
     ticker.style.animation = "none";
-    // запускаем анимацию вращение
-    runTickerAnimation();
-  });
-  
-  // отслеживаем, когда закончилась анимация вращения колеса
-  spinner.addEventListener("transitionend", () => {
-    // останавливаем отрисовку вращения
-    cancelAnimationFrame(tickerAnim);
-    // получаем текущее значение поворота колеса
-    rotation %= 360;
-    // выбираем приз
-    selectPrize();
-    // убираем класс, который отвечает за вращение
-    wheel.classList.remove(spinClass);
-    // отправляем в CSS новое положение поворота колеса
-    spinner.style.setProperty("--rotate", rotation);
-    // делаем кнопку снова активной
-    trigger.disabled = false;
-  });
-  
-  // подготавливаем всё к первому запуску
-  setupWheel();
+    setTimeout(() => ticker.style.animation = null, 10);
+    currentSlice = slice;
+  }
+  tickerAnim = requestAnimationFrame(runTickerAnimation);
+};
+
+const selectPrize = () => {
+  const selected = Math.floor(rotation / prizeSlice) % prizes.length;
+  prizeNodes[selected].classList.add(selectedClass);
+};
+
+trigger.addEventListener("click", () => {
+  trigger.disabled = true;
+
+  if (spinCount === 0) {
+    rotation += 360 * 5 + (prizeSlice * 1) + prizeOffset; // Spin Again
+  } else if (spinCount === 1) {
+    rotation += 360 * 5 + (prizeSlice * 6) + prizeOffset + 5; // 500 ZWM
+  } else {
+    rotation += 360 * 5 + Math.floor(Math.random() * 360); // Random for subsequent spins
+  }
+  spinCount++;
+
+  prizeNodes.forEach((prize) => prize.classList.remove(selectedClass));
+  wheel.classList.add(spinClass);
+  spinner.style.setProperty("--rotate", rotation);
+  ticker.style.animation = "none";
+  runTickerAnimation();
+});
+
+spinner.addEventListener("transitionend", () => {
+  cancelAnimationFrame(tickerAnim);
+  rotation %= 360;
+  selectPrize();
+  wheel.classList.remove(spinClass);
+  spinner.style.setProperty("--rotate", rotation);
+  trigger.disabled = false;
+});
+
+setupWheel();
